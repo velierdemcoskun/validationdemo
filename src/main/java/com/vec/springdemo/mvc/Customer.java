5412af5a92c31bf13dbc6c15f57960e301798f5b
velierdemcoskun <erdemcoskun0619@gmail.com>
2023-08-19 18:05:34 +0300

    final version

---
+++ src/main/java/com/vec/springdemo/mvc/Customer.java
@@ -0,0 +1,66 @@
+package com.vec.springdemo.mvc;
+
+import com.vec.springdemo.mvc.validation.CourseCode;
+import jakarta.validation.constraints.*;
+
+public class Customer {
+
+    private String firstName;
+
+    @NotNull(message = "is required")
+    @Size(min=1, message = "is required")
+    private String lastName = "";
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 characters/digits")
+    private String postalCode;
+
+
+    @CourseCode
+    private String courseCode;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+}
---
+++ src/main/java/com/vec/springdemo/mvc/ValidationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.vec.springdemo.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationdemoApplication.class, args);
+	}
+
+}
